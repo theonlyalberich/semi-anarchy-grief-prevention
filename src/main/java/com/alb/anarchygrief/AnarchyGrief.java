3850a2028ba60d4204f42cfa720c16ddbc8d07eb
theonlyalberich <the1.hendrik1@gmail.com>
2026-08-24 01:41:50 +0200

    Implement global protection on startup
    Added global protection enforcement on startup.

--- src/main/java/com/alb/anarchygrief/AnarchyGrief.java
+++ src/main/java/com/alb/anarchygrief/AnarchyGrief.java
@@ -4,6 +4,7 @@ import com.alb.anarchygrief.listeners.ConnectionHandler;
 import com.alb.anarchygrief.listeners.PlayerConnectionListener;
 import com.alb.anarchygrief.triggers.DisableProtection;
 import com.alb.anarchygrief.triggers.EnableProtection;
+import com.alb.anarchygrief.triggers.GlobalProtectionEnable;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -37,10 +38,15 @@ public class AnarchyGrief extends JavaPlugin implements ConnectionHandler {
         // Managers
         DisableProtection disabler = new DisableProtection(gp.dataStore);
         EnableProtection enabler = new EnableProtection(gp.dataStore);
+        GlobalProtectionEnable globalEnabler = new GlobalProtectionEnable(gp.dataStore);
 
         // Configurable delays
         int loginDelayMinutes = getConfig().getInt("login-delay-minutes", 5);
         int restoreDelayMinutes = getConfig().getInt("restoreDelayMinutes", 5);
+
+        // Startup safeguard: enforce protection globally
+        globalEnabler.enableProtectionOnAllClaims();
+        getLogger().info("Global protection enforced on startup.");
 
         // Register listener with both managers
         getServer().getPluginManager().registerEvents(
